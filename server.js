27015aa369014c4e82c048fba2ec20d6fb15909b
Anamika Jha <anamika.jha.cse@gmail.com>
2026-06-27 17:38:06 +0530

    adding the nodejs project of retro stuff

---
+++ server.js
@@ -0,0 +1,13 @@
+import http from 'node:http'
+
+const PORT = 8000
+
+console.log(import.meta.dirname)
+
+const server = http.createServer((req, res)=> {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html')
+  res.end()
+})
+
+server.listen(PORT, () => console.log('connected on port 8000') )
